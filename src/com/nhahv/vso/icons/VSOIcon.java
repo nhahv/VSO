143c309e430ad566e2d67675a8b33ad3026e1343
Nha.HV <nhahv.viss@gmail.com>
2015-07-29 03:06:03 +0700

    Initial commit

---
+++ src/com/nhahv/vso/icons/VSOIcon.java
@@ -0,0 +1,13 @@
+package com.nhahv.vso.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Created by nhahv on 7/29/15.
+ */
+public class VSOIcon {
+    public static final Icon VSOICON = IconLoader.getIcon("/icons/favicon.png"); // 16x16
+
+}
